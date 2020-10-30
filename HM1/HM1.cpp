133ead550e7a7d4fc87de73428c7473d7d9494b1
Qvocha <43353220+Qvocha@users.noreply.github.com>
2020-10-30 17:00:09 +0300

    end

--- HM1/HM1.cpp
+++ HM1/HM1.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-	int r;
-	int e;
-	int y;
-	int u;
-	int i;
+	int r = 0;
+	int e = 0;
+	int y = 0;
+	int u = 0;
+	int i = 0;
 	cin >> r >> e >> y >> u >> i;
 	cout << r * e * y * u * i;
 	return 0;
